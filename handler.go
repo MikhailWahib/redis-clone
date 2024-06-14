3209e28679e8927c4b295f33c5790d06fc32ca8c
MikhailWahib <mikhailwahib1@gmail.com>
2024-06-14 10:36:51 +0300

    Add PING cmd handler

---
+++ handler.go
@@ -0,0 +1,18 @@
+package main
+
+var Handlers = map[string]func([]Value) Value{
+	"PING": ping,
+	// "SET":     set,
+	// "GET":     get,
+	// "HSET":    hset,
+	// "HGET":    hget,
+	// "HGETALL": hgetall,
+}
+
+func ping(args []Value) Value {
+	if len(args) == 0 {
+		return Value{typ: "string", str: "PONG"}
+	}
+
+	return Value{typ: "string", str: args[0].bulk}
+}
