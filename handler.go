b83a4780f768a0dfc02e0df4de76f3a55abdb18a
MikhailWahib <mikhailwahib1@gmail.com>
2024-06-14 11:47:37 +0300

    Add HGET, HSET and HGETALL cmds handlers

--- handler.go
+++ handler.go
@@ -1,14 +1,16 @@
 package main
 
-import "sync"
+import (
+	"sync"
+)
 
 var Handlers = map[string]func([]Value) Value{
-	"PING": ping,
-	"SET":  set,
-	"GET":  get,
-	// "HSET":    hset,
-	// "HGET":    hget,
-	// "HGETALL": hgetall,
+	"PING":    ping,
+	"SET":     set,
+	"GET":     get,
+	"HSET":    hset,
+	"HGET":    hget,
+	"HGETALL": hgetall,
 }
 
 func ping(args []Value) Value {
@@ -41,8 +43,8 @@ func get(args []Value) Value {
 	if len(args) != 1 {
 		return Value{typ: "error", str: "ERR wrong number of arguments for 'set' command"}
 	}
-	SETsMu.RLock()
-	defer SETsMu.RUnlock()
+	SETsMu.Lock()
+	defer SETsMu.Unlock()
 
 	key := args[0].bulk
 
@@ -52,4 +54,70 @@ func get(args []Value) Value {
 	}
 
 	return Value{typ: "bulk", bulk: value}
+}
+
+var HSETs = map[string]map[string]string{}
+var HSETsMu = sync.RWMutex{}
+
+func hset(args []Value) Value {
+	if len(args) != 3 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'hset' command"}
+	}
+
+	hash := args[0].bulk
+	key := args[1].bulk
+	value := args[2].bulk
+
+	HSETsMu.Lock()
+	defer HSETsMu.Unlock()
+
+	if _, ok := HSETs[hash]; !ok {
+		HSETs[hash] = map[string]string{}
+	}
+	HSETs[hash][key] = value
+
+	return Value{typ: "string", str: "OK"}
+}
+
+func hget(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'hget' command"}
+	}
+
+	hash := args[0].bulk
+	key := args[1].bulk
+
+	HSETsMu.Lock()
+	defer HSETsMu.Unlock()
+
+	value, ok := HSETs[hash][key]
+	if !ok {
+		return Value{typ: "null"}
+	}
+
+	return Value{typ: "bulk", bulk: value}
+}
+
+func hgetall(args []Value) Value {
+	if len(args) != 1 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'hgetall' command"}
+	}
+
+	hash := args[0].bulk
+
+	HSETsMu.Lock()
+	defer HSETsMu.Unlock()
+
+	value, ok := HSETs[hash]
+	if !ok {
+		return Value{typ: "null"}
+	}
+
+	values := []Value{}
+	for k, v := range value {
+		values = append(values, Value{typ: "bulk", bulk: k})
+		values = append(values, Value{typ: "bulk", bulk: v})
+	}
+
+	return Value{typ: "array", array: values}
 }
